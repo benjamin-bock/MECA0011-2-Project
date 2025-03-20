585ae6405d7e0bdb9fcf9e37f3cedb39df30ef62
Re3syyy <baptiste.desmedt@icloud.com>
2025-03-20 16:02:57 +0100

    intiale

---
+++ getCoeff.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 20 15:59:13 2025
+
+@author: baptiste
+"""
+import numpy as np 
+import scipy as sc
+
+def getCoeff(num_left, num_right, num_down, num_up, num_cent, type_cent, cl_cent) :
+    
