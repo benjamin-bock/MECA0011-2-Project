55c2b229f5a16ff95381a490bd4993a61e80dacf
Re3syyy <baptiste.desmedt@icloud.com>
2025-03-20 17:25:19 +0100

    getCoeff

--- getCoeff.py
+++ getCoeff.py
@@ -10,16 +10,18 @@ import scipy as sc
 
 #return j, a, b
 
-def getCoeff(num_left, num_right, num_down, num_up, num_cent, type_cent, cl_cent) :
-    if(type_cent == 0) :
+def getCoeff(num_left, num_right, num_down, num_up, num_cent, type_cent, cl_cent):
+    if type_cent == 0:
         raise ValueError("Invalid value for type_cent: 0 is not in the domain of calcul.")
-        return 0, 0, 0
-    if(type_cent == 2) :
-        a = np.array(1)
-        j = np.array(num_cent)
-        return j, a, cl_cent
-    if(type_cent == 1) :
-        j = np.array([num_left, num_right, num_down, num_up, num_cent])
-        a = np.array([1, 1, 1, 1, -4])
-        return j, a, cl_cent
- 
+    
+    if type_cent == 2:
+        a = np.array([1]).reshape(-1,1)
+        j = np.array([num_cent]).reshape(-1,1)
+        b = cl_cent
+    
+    if type_cent == 1:
+        j = np.array([num_left, num_right, num_down, num_up, num_cent]).reshape(-1,1)  # Transpose the array
+        a = np.array([1, 1, 1, 1, -4]).reshape(-1,1)
+        b = 0
+    
+    return j , a , b
