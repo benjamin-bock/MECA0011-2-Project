d27a85c3b26e190ad49c819bf4ac10f2dbf176a9
Benjamin Bock <benjaminbock05@gmail.com>
2025-03-20 19:22:28 +0100

    feat: add laplacian

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+
+def main():
+
+    dom_1 = np.loadtxt('CL\1-dom.txt', dtype = int)
+    cl_1 = np.loadtxt('CL\1-cl.txt', dtype = int)
+    num_1 = np.loadtxt('CL\1-num.txt', dtype = int)
+
+    
