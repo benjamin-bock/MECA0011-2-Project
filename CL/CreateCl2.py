82062d06c91f9983f00055aa3743675082f3df2d
Re3syyy <baptiste.desmedt@icloud.com>
2025-04-15 21:50:54 +0200

    CreateCl2

--- CL/CreateCl2.py
+++ CL/CreateCl2.py
@@ -2,14 +2,10 @@ import numpy as np
 
 # Load the domain matrix
 dom_matrix = np.loadtxt('2-dom.txt', dtype=int)
-contour = np.loadtxt('2-contourObj.txt', dtype = int)
+contour = np.loadtxt('2-contourObj.txt', dtype=int)
+
 # Create the cl1 matrix based on the domain
-cl1_matrix = np.zeros(dom_matrix.shape, dtype=float)
-
-
-cl1_matrix[dom_matrix == 2] = 0.00000000
-cl1_matrix[dom_matrix ==  1] = 0.00000000
-
+CL = np.zeros(dom_matrix.shape, dtype=float)
 
 X = 0   # X is the tens digit of the group number
 Y = 1  # Y is the units digit of the group number
@@ -17,21 +13,60 @@ h = 0.01
 Q_out = (10 * X + 5 * Y) * 10**(-3)  
 Q_in = Q_out / 2 
 
-y = np.arange(0,60 *h,60 )
+L = 21*h
+u_intlet = Q_in/L
+u_outlet = Q_out/L
 
-cl1_matrix[1, 1:22] = -Q_out
-cl1_matrix[1, 79:100] = Q_in
-cl1_matrix[99, 79:100] = Q_in
+# 2 entrées du canal 
+for i in range(0,21):
+    CL[1][79 + i] = -u_intlet * h * i  
+    CL[99][79 + i] = u_intlet * h * i
 
-for index in contour:
-    cl1_matrix[tuple(index)] = Q_in
+#2 canal d'entré interieu 
+CL[1:41,79] = 0.000
+CL[60:100,79] = 0.00
+
+#canal d'entré exterieur
+CL[1:41,99] = -u_intlet*h*21
+CL[41:100,99] = u_intlet*h*21
+
+#sortie du canal
+for i in range(0,20): 
+    CL[1][i+1] = u_outlet*h*i
     
+CL[1:61, 1] = 0.0000
+
+# Apply fully developed flow conditions
+for index in contour:
+    if index[0] >=50: 
+        CL[tuple(index)] = Q_in/2
+    if index[0]  < 50: 
+        if index[1] >= 32: 
+            CL[tuple(index)] = - Q_in/2
+        if index[1] < 32 : 
+            CL[tuple(index)] = Q_in/2
+    
+CL[1:41, 21] = u_outlet*h*21
+
+CL[40,21:79] = u_intlet*h*21
+CL[11:40,86] = -u_intlet*h*21
+
+CL[39:42,85] = -u_intlet*h*21
+
+CL[41:43,84] = -u_intlet*h*21
+CL[42:44,83] = -u_intlet*h*21
+
+CL[43:45,82] = -u_intlet*h*21
 
 
+
+
+
+
+# Save the boundary conditions to a file
 with open('2-cl.txt', 'w') as f:
-    for row in cl1_matrix:
+    for row in CL: 
         formatted_row = '   '.join(f'{val:.8f}' for val in row)
         f.write(f'{formatted_row}\n')
 
 print("Boundary conditions have been set successfully.")
-print(contour)
